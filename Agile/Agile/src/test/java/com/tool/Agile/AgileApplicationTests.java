6480363dd62f3dc2900bcb61076bc476996956ae
unknown <simli@gmail.com>
2020-09-10 15:23:27 +0530

    first commit

---
+++ Agile/Agile/src/test/java/com/tool/Agile/AgileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tool.Agile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
